4a9125c46e59c82ad12a918d55b6e852a2d9815b
@rezabayu <rezabayuperdana@gmail.com>
2025-05-23 17:35:45 +0700

    To-Do: Create Simple register and login page.

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 from flask_login import UserMixin
+from flask_login import LoginManager
+from forms import RegistrationForm, LoginForm
+from flask import flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.secret_key = 'irvina'
 
 # Configuring the SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
@@ -13,10 +19,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 # User Model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
 
 # Database model
 class Task(db.Model):
@@ -25,6 +36,7 @@ class Task(db.Model):
     timestamp = db.Column(db.DateTime, default=datetime.now)
     is_completed = db.Column(db.Boolean, default=False)
     completed_at = db.Column(db.DateTime, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"<Task {self.content}>"
@@ -37,6 +49,10 @@ class Task(db.Model):
 #         task.timestamp = saiki
 #     db.session.commit()
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -47,19 +63,19 @@ def index():
             db.session.commit()
             return redirect(url_for('index'))
 
-    tasks = Task.query.all()
+    tasks = Task.query.filter_by(user_id=current_user.id).all()
     return render_template('index.html', tasks=tasks)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Task.query.get_or_404(id)
+    task_to_delete = Task.query.filter_by(id=id, user_id=current_user.id).first_or_404()
     db.session.delete(task_to_delete)
     db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
-    task = Task.query.get_or_404(id)
+    task = Task.query.filter_by(id=id, user_id=current_user.id).first_or_404()
     if request.method == 'POST':
         task.content = request.form['content']
         db.session.commit()
@@ -69,7 +85,7 @@ def edit(id):
 
 @app.route('/toggle/<int:id>')
 def toggle(id):
-    task = Task.query.get_or_404(id)
+    task = Task.query.filter_by(id=id, user_id=current_user.id).first_or_404()
     if task.is_completed:
         task.is_completed = False
         task.completed_at = None
@@ -78,6 +94,40 @@ def toggle(id):
         task.completed_at = datetime.now()
     db.session.commit()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(username=form.username.data).first()
+        if existing_user:
+            flash('Username already exists. Choose another.')
+            return redirect(url_for('register'))
+        hashed_pw = generate_password_hash(form.password.data)
+        user = User(username=form.username.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration succesful!\nYou can log in now.')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('index'))
+        flash('Invalid username or password.')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     with app.app_context():
